bac639d8aa906bc79813b8215c6bbf62ac1b42c3
NviCoder <NviCoder@example.com>
2021-02-10 13:57:56 +0200

    The table are ready for dis function

--- ramzorCpa.py
+++ ramzorCpa.py
@@ -5,45 +5,70 @@ import math
 #from sklearn.decomposition import PCA
 #from sklearn.pipeline import make_pipeline
 
+
 #distances for city X
-CITY_NAME = "TEL AVIV - YAFO"
+CITY_CODE = "5000"
 DAT = 4
-COORDINATES = 1802766486 
-HEIGHT = 17
+COORDINATES1 = 18027
+COORDINATES2 = 66486 
 
-
-#disnatce function
-def get_distance(dat, popTot, coordinates, height):
-    return HEIGHT - height
+#Distance function
+def l_2(x, y):
+        return math.sqrt(sum(map(lambda a, b: (a - b) ** 2, x, y)))
 
 
 #Clean the population.csv file
 data_population = pd.read_csv("population.csv", sep=",", header=0)
-data_population.drop(["nameH", "city_code", "mahoz", "napa" ,"ezor tivee","maamad","shiyuch"
-                     ,"Yisood", "tzoora", "irgun", "vaada", "police", "year", "nameE2", "eshcol"],axis='columns', inplace=True)
+data_population.drop(["nameH","nameE1", "mahoz", "napa" ,"ezor tivee","maamad","shiyuch"
+                     ,"Yisood", "tzoora", "irgun", "vaada", "police", "year", "nameE2", "eshcol","height","popJewish"],axis='columns', inplace=True)
 
 #drop enpty rows
-#rows that we have at the csv file
-#*nameE1*	*dat*	*popTot*	*popNotArab*	*popJewish*	*popArab*	*coordinates*	*height*
+#rows that we have at the csv file                                   0123456789
+#*city_code*	*dat*	*popTot* *popNotArab* *popArab*	*coordinates*1234512345
+
+data_population = data_population.dropna(subset=['city_code'])
+data_population = data_population.dropna(subset=['popTot'])
 data_population = data_population.dropna(subset=['dat'])
+data_population = data_population.dropna(subset=['coordinates'])
+
 
 #Add new row for distance
+data_population['coordinates1']=""
+data_population['coordinates2']=""
 data_population['distance']=0.0
 
-#calculate the dis
+
+#Split coordinates for each city
 for index, row in data_population.iterrows():
     
-    current_city_height = row['height']
-    if current_city_height == " ":
-        data_population.at[index, 'distance'] = pd.NA
-    else:                                                   
-        data_population.at[index, 'distance'] = get_distance(dat=data_population.at[index, 'dat'], popTot=data_population.at[index, 'popTot'],
-          coordinates=data_population.at[index, 'coordinates'], height=data_population.at[index, 'height'])
+    current_city_popNotArab = row['popNotArab']
+    current_city_popArab= row['popArab']
+    print(current_city_popArab)
+    if  math.isnan(current_city_popNotArab) or current_city_popNotArab == "":
+        if math.isnan(current_city_popArab) or current_city_popArab == "": #current_city_popNotArab =0 current_city_popArab=0
+            data_population.drop([index,index])
+            continue
+        else: #current_city_popNotArab =0 current_city_popArab=1
+            data_population.at[index, 'popNotArab'] = 0.0
+    else:
+         if math.isnan(current_city_popArab) or current_city_popArab == "":#current_city_popNotArab =1 current_city_popArab=0
+             data_population.at[index, 'popArab'] = 0.0
+        #else - current_city_popNotArab =1 current_city_popArab=1
+            
+    current_city_coordinates = row['coordinates']                                           
+    data_population.at[index, 'coordinates1'] = str(current_city_coordinates)[0:5]
+    data_population.at[index, 'coordinates2'] = str(current_city_coordinates)[5:10]      
 
+#Drop coordinates colomn
+data_population.drop(["coordinates"],axis='columns', inplace=True)
 
+'''
+#Exem the function
+for index, row in data_population.iterrows():
+    data_population.at[index, 'distance'] = l_2(COORDINATES1,row['coordinates1']) + l_2(COORDINATES2,row['coordinates2'])
+'''
 
-
-
+#Generate the csv file
 data_population.to_csv('population_for_cpa.csv', sep=',')
 print("sucsses!")
 
