df0b1cba736112f0e86646cef930eb646773f5b3
NviCoder <NviCoder@example.com>
2021-02-14 11:36:38 +0200

    loop for results

--- time_series.py
+++ time_series.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import math
+import random
 from matplotlib import pyplot
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
@@ -11,24 +12,29 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 
-#select params
-city_code = 3000
+# select params
+#city_code = 3000
 window_size = 3
 look_forward = 1
 train_ratio = 0.75
-knn = 1 # set -1 for all the cities
+knn = 1  # set -1 for all the cities
 split_date = None
 adaptive = False
+
+
+
+
 
 #model_type = "random forest"
 model_type = "linear regression"
 #model_type = "svm"
 #model_type = "lstm"
 
+
 def select_model(model_type):
     if model_type == "random forest":
         return RandomForestRegressor(n_estimators=100, criterion='mse',
-                                      min_samples_split=math.ceil(math.log2(trainX.size)))
+                                     min_samples_split=math.ceil(math.log2(trainX.size)))
     elif model_type == "linear regression":
         return LinearRegression()
     elif model_type == "svm":
@@ -43,11 +49,24 @@ def select_model(model_type):
     print("model_type error")
     return None
 
-#read data set
+
+# read data set
 ds = pd.read_csv("ramzor2.csv", sep=",", header=0)
 proc_pop = pd.read_csv("population_for_cpa.csv", sep=",", header=0)
-proc_pop = proc_pop.drop(proc_pop[~proc_pop.city_code.isin(ds.City_Code)].index)
+proc_pop = proc_pop.drop(
+    proc_pop[~proc_pop.city_code.isin(ds.City_Code)].index)
 proc_pop.set_index('city_code', inplace=True)
+
+#for random cities
+def get_all_cities_list():
+    return proc_pop.index.values.tolist()
+
+#Random cities
+all_cities = get_all_cities_list()
+random_cities_list = random.sample(all_cities,20)
+print(random_cities_list)
+
+
 
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     """
@@ -82,6 +101,7 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
         agg.dropna(inplace=True)
     return agg
 
+
 def split_train_test(data, train_ratio, look_forward):
     """
     Split to train and test by ratio.
@@ -95,9 +115,14 @@ def split_train_test(data, train_ratio, look_forward):
     X = data.values
     train_size = int(len(X) * train_ratio)
     train, test = X[0:train_size], X[train_size:len(X)]
-    return train[:, :-look_forward], train[:, -1], test[:, :-look_forward], test[:, -1] #trainX, trainy, testX, testy
-    #return train[:, :-1], train[:, -1], test[:, :-1], test[:, -1] #trainX, trainy, testX, testy
-filter_by_city = lambda city_code: ds[ds['City_Code'] == city_code].copy()
+    # trainX, trainy, testX, testy
+    return train[:, :-look_forward], train[:, -1], test[:, :-look_forward], test[:, -1]
+
+    # return train[:, :-1], train[:, -1], test[:, :-1], test[:, -1] #trainX, trainy, testX, testy
+
+
+def filter_by_city(city_code): return ds[ds['City_Code'] == city_code].copy()
+
 
 def predict_static(model, testX):
     """
@@ -109,6 +134,7 @@ def predict_static(model, testX):
     for i in range(len(testX)):
         predictions.append(model.predict(testX[i].reshape(1, -1)))
     return predictions
+
 
 def predict_adaptive(model, trainX, trainy, testX, testy):
     """
@@ -128,20 +154,25 @@ def predict_adaptive(model, trainX, trainy, testX, testy):
         model.fit(trainX, trainy)
     return predictions
 
+
 class AsLastColumn:
     def __init__(self):
         pass
+
     def fit(self):
         pass
-    def predict(self, x):
-        return x[:,-1]
 
-def get_all_cities_list():
-    return proc_pop.index.values.tolist()
+    def predict(self, x):
+        return x[:, -1]
+
+
+
+
 
 def one_city_train_test(city_code):
     ds_city = filter_by_city(city_code)
-    ds_city.drop(['index','City_Code','Cumulative_verified_cases','Cumulated_number_of_tests','Ni','Pi','Gi'], axis='columns', inplace=True)
+    ds_city.drop(['index', 'City_Code', 'Cumulative_verified_cases',
+                  'Cumulated_number_of_tests', 'Ni', 'Pi', 'Gi'], axis='columns', inplace=True)
 
     # First time: set split date by the ratio. More times: set ratio by split date.
     global split_date
@@ -153,29 +184,36 @@ def one_city_train_test(city_code):
         if not test_list:
             current_ratio = 0
         else:
-            fist_test_row = ds_city.index[ds_city['Date'] == split_date].to_list()[0]
-            current_ratio = (fist_test_row - ds_city.index.to_list()[0]) / float(len(ds_city))
+            fist_test_row = ds_city.index[ds_city['Date'] == split_date].to_list()[
+                0]
+            current_ratio = (
+                fist_test_row - ds_city.index.to_list()[0]) / float(len(ds_city))
 
     ds_city.set_index('Date', inplace=True)
     ds_city = series_to_supervised(ds_city, window_size, look_forward)
     return split_train_test(ds_city, current_ratio, look_forward)
 
+
 def all_cities_train():
     return train_by_cities_list(get_all_cities_list())
+
 
 def l2_cities(city_code1, city_code2):
     vec1 = proc_pop.loc[city_code1]
     vec2 = proc_pop.loc[city_code2]
     return mean_squared_error(vec1, vec2)
 
+
 def knn_cities_train(city_code, top_k):
     cities = get_all_cities_list()
     if top_k > len(cities):
         return all_cities_train()
-    sort_by_dis = sorted(enumerate(cities), key=(lambda y: l2_cities(y[1], city_code)))
+    sort_by_dis = sorted(enumerate(cities), key=(
+        lambda y: l2_cities(y[1], city_code)))
     head_of_list = [sort_by_dis[i][1] for i in range(top_k)]
     print(top_k, "nearest neighbor cities:", head_of_list)
     return train_by_cities_list(head_of_list)
+
 
 def train_by_cities_list(cities):
     trainX, trainy = np.empty(shape=(0, window_size)), np.empty(shape=(1, 0))
@@ -185,47 +223,56 @@ def train_by_cities_list(cities):
         trainy = np.append(trainy, trainy_city)
     return trainX, trainy
 
+counter = 1
 
-# Split train-test
-trainX, trainy, testX, testy = one_city_train_test(city_code)
-if knn == -1:
-    trainX, trainy = all_cities_train()
-elif knn > 1:
-    trainX, trainy = knn_cities_train(city_code, knn)
+for city_code in random_cities_list:
+    print("=======================================================================================")
+    # Split train-test
+    trainX, trainy, testX, testy = one_city_train_test(city_code)
+    if knn == -1:
+        trainX, trainy = all_cities_train()
+    elif knn > 1:
+        trainX, trainy = knn_cities_train(city_code, knn)
 
-# Select model and learn
-model = select_model(model_type)
+    # Select model and learn
+    model = select_model(model_type)
 
-if model_type == "lstm":
-    # reshape input to be [samples, time steps, features]
-    print(window_size, trainX.shape[0], 1)
-    trainX = np.reshape(trainX, (window_size, trainX.shape[0], 1))
-    testX = np.reshape(testX, (window_size, testX.shape[0], 1))
+    if model_type == "lstm":
+        # reshape input to be [samples, time steps, features]
+        print(window_size, trainX.shape[0], 1)
+        trainX = np.reshape(trainX, (window_size, trainX.shape[0], 1))
+        testX = np.reshape(testX, (window_size, testX.shape[0], 1))
 
-    model.fit(trainX, trainy, epochs=5, batch_size=1, verbose=2)
-else:
-    model.fit(trainX, trainy)
-if model_type == "random forest":
-    print('importances', model.feature_importances_) # optional to RandomForestRegressor
+        model.fit(trainX, trainy, epochs=5, batch_size=1, verbose=2)
+    else:
+        model.fit(trainX, trainy)
+    if model_type == "random forest":
+        # optional to RandomForestRegressor
+        print('importances', model.feature_importances_)
 
-#prediction
-if adaptive:
-    predictions = predict_adaptive(model, trainX, trainy, testX, testy)
-else:
-    predictions = predict_static(model, testX)
+    #Iteration number  + city code
 
-error = mean_squared_error(testy, predictions)
-print("model error:", error)
+    print("Iteration: ",counter," For city: ",city_code)
+    # prediction
+    if adaptive:
+        predictions = predict_adaptive(model, trainX, trainy, testX, testy)
+    else:
+        predictions = predict_static(model, testX)
 
-# monkey model learning
-model_monkey = AsLastColumn()
-predictions_monkey = predict_static(model_monkey, testX)
-error_monkey = mean_squared_error(testy, predictions_monkey)
-print("monkey error:", error_monkey)
+    error = mean_squared_error(testy, predictions)
+    print("model error:", error)
 
-# plot expected vs predicted vs monkey
-pyplot.plot(testy, label='Expected')
-pyplot.plot(predictions, label='Predicted')
-pyplot.plot(predictions_monkey, label='Monkey')
-pyplot.legend()
-pyplot.show()
+    # monkey model learning
+    model_monkey = AsLastColumn()
+    predictions_monkey = predict_static(model_monkey, testX)
+    error_monkey = mean_squared_error(testy, predictions_monkey)
+    print("monkey error:", error_monkey)
+    counter = counter + 1
+    # plot expected vs predicted vs monkey
+    '''
+    pyplot.plot(testy, label='Expected')
+    pyplot.plot(predictions, label='Predicted')
+    pyplot.plot(predictions_monkey, label='Monkey')
+    pyplot.legend()
+    pyplot.show()
+    '''
